547468b976a93b54bdeb79ebb90bff3819d1009d
Timmy Le <le.timmy94@gmail.com>
2018-03-07 12:26:58 -0500

    changed link color

--- db/connection.js
+++ db/connection.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 if (process.env.NODE_ENV == "production") {
   mongoose.connect(process.env.MLAB_URL);
 } else {
-  mongoose.connect("mongodb://localhost/puppys");
+  mongoose.connect("mongodb://localhost/pupperz");
 }
 
 mongoose.Promise = Promise;
